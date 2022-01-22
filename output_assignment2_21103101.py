130add1f82e836653ba4040697e254ff26525775
pb1705 <97444456+pb1705@users.noreply.github.com>
2022-01-22 12:42:07 +0530

    Add files via upload

---
+++ output_assignment2_21103101.py
@@ -0,0 +1,36 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:\Users\hp\OneDrive\Desktop\ASSIGNMENTS (1)\COMPUTER PRACTICAL\ASSIGNMENT2_21103101_PrranavBabbar.py
+a) The length of given string is  35
+b) The reversed string is 
+egaugnal evitisnes esac a si nohtyP
+e) The index of 'a' is : 10
+f) The given string with no spaces :
+Pythonisacasesensitivelanguage
+Hey,Prranav Babbar Here!
+MY SID is 21103101
+I am from CSE department and my CGPA is 9.900000
+
+a) The value of bitwise a and b = 8
+b) The value of bitwise a or b =  58
+c) The value of a xor b is  50
+d) The values of both a and b shifted left bitwise by 2 bits are  224 and  40  respectively
+e) The value of a shifted right bitwise by 2 bits is  14 and The value of b shifted right bitwise by 4 bits is  0
+
+
+Enter the first number :
+35
+Enter the second number :
+65
+Enter the third number :
+45
+The largest number you have entered is 65 
+
+Enter a string to search for word 'name' in it:
+MY name is KHAN
+Yes
+Enter the first side  of triangle :3
+Enter the second side  of triangle :4
+Enter the third side  of triangle :5
+YES! You can construct a TRIANGLE with the data you entered

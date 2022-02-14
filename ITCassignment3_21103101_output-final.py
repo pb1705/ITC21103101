518dfc1f7b7997f32a96e1b51aebb0877aa1ccad
pb1705 <97444456+pb1705@users.noreply.github.com>
2022-02-14 15:28:16 +0530

    Add files via upload

---
+++ ITCassignment3_21103101_output-final.py
@@ -0,0 +1,69 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+========= RESTART: C:\Users\hp\OneDrive\Desktop\assignment3_21103101.py ========
+Enter the string you want to get the count of words Hello
+{'h': 1, 'e': 1, 'l': 2, 'o': 1}
+Enter the day corresponding to your date28
+Enter the month corresponding to your date02
+Enter the year corresponding to your date1800
+Next date is : 01/03/1800
+Enter the total number of inputs you want to give5
+Enter the 1th number you want square of
+3
+Enter the 2th number you want square of
+6
+Enter the 3th number you want square of
+9
+Enter the 4th number you want square of
+10
+Enter the 5th number you want square of
+4
+[(3, 9), (6, 36), (9, 81), (10, 100), (4, 16)]
+Please enter your 'grade points':10
+Your Grade is 'A+' and Outstanding Performance
+ABCDEFGHIJK
+ ABCDEFGHI
+  ABCDEFG
+   ABCDE
+    ABC
+     A
+
+
+Enter the sid of student
+21103101
+Enter the name of the student
+Prranav
+Do you want to continue enter'Y' to continue and 'N' to exit
+Y
+Enter the sid of student
+21103101
+Enter the name of the student
+Noone
+Error! SID is already stored
+Do you want to continue enter'Y' to continue and 'N' to exit
+Y
+Enter the sid of student
+21104081
+Enter the name of the student
+Ravinoor
+Do you want to continue enter'Y' to continue and 'N' to exit
+Y
+Enter the sid of student
+21105106
+Enter the name of the student
+Himanshu
+Do you want to continue enter'Y' to continue and 'N' to exit
+N
+a) {21103101: 'Prranav', 21104081: 'Ravinoor', 21105106: 'Himanshu'}
+b) {21105106: 'Himanshu', 21103101: 'Prranav', 21104081: 'Ravinoor'}
+c) {21103101: 'Prranav', 21104081: 'Ravinoor', 21105106: 'Himanshu'}
+Enter the SID to search for the name of the student 21103101
+d) Prranav
+Enter the number you want the fibonacci series upto10
+0 1 1 2 3 5 8 13 21 34 
+a) {1, 3, 5, 6, 8}
+b) {3, 6, 8, 9, 13, 17}
+c) {1, 2, 4, 5}
+d) {6, 7, 8, 9, 10}
+e) {10, 7}
